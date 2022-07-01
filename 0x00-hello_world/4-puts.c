366ebcc900a104617958033fe9bdcdb44216d6d7
waheedibrahim <a.waheedibrahim@gmail.com>
2022-07-01 06:59:24 -0700

    commt

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-/*
-*printing a simple program using puts function
+/**
 * main -Entry point
 * Return: Always 0 (success)
 */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-/* main itneger from source*/
-
 return (0);
 }
