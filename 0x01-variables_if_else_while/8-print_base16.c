0d8f9e6e1c45db1814d0a45ff75daf5b8a03501d
waheedibrahim <a.waheedibrahim@gmail.com>
2022-07-01 21:57:17 -0700

    commit for c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,12 +6,12 @@
 *main - prints all the numbers of base 16 in lowercase, followed by a new line
 *
 *Return: 0
-**/
+*/
 
 int main(void)
 {
 /**
-*
+
 * hexadecimal
 * 0 1 2 3 4 5 6 7 8 9 a c d e f
 */
