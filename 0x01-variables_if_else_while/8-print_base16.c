1da9b26eb8b4e6036429748cbff5eb03f43b1128
waheedibrahim <a.waheedibrahim@gmail.com>
2022-07-03 17:00:29 -0700

    committ

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,15 +4,11 @@
 
 /**
 *main - prints all the numbers of base 16 in lowercase, followed by a new line
-*
-*Return: 0
-**/
+*Return: Always return 0
+*/
+
 int main(void)
 {
-
-/**
-*hexadecimal 0 1 2 3 4 5 6 7 8 9 a c d e f
-*/
 	
 char ch;
 int n;
