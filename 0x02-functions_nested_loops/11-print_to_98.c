f15a642aba5b18dfc0131bc1a93c5fb51db5e699
waheedibrahim <a.waheedibrahim@gmail.com>
2022-07-06 15:48:13 -0700

    ading alphabet question11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
 * print_to_98 - prints all natural numbers from n to 98,
 * followed by a new line
 * @n: print from this number
 */
+
 void print_to_98(int n)
 {
 int i, j;
@@ -24,11 +24,12 @@ else if (n >= 98)
 { 
 	
 for (j = n; j >= 98; j--)
-}
+
 {
 if (j != 98)
 printf("%d, ", j);
 else if (j == 98)
+{
 printf("%d\n", j);
 }
 }
