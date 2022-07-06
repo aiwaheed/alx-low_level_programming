62823473559a4a1090af1eef91b692ae3b5667f1
waheedibrahim <a.waheedibrahim@gmail.com>
2022-07-06 16:41:39 -0700

    commits 104fibonaccis

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,10 +1,10 @@
 #include "main.h"
+
 /**
 * times_table - prints the 9 times table, starting with 0
 */
 void times_table(void)
 {
-
 int i, j, k;
 for (i = 0; i < 10; i++)
 {
@@ -30,9 +30,7 @@ _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
 }
-{
+}
 _putchar('\n');
-}
-}
 }
 }
