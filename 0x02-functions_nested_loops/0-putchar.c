5200ebba8495782ea0cd945ef0d904d9da809984
waheedibrahim <a.waheedibrahim@gmail.com>
2022-07-05 20:45:38 -0700

    commit requirements

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,17 +5,17 @@
 *
 * Return: 0 (Success)
 */
+
 int main(void)
 {
-int i = 0;
-char text[] = "_putchar";
-
-while (text[i] != '\0')
-#endif
-{
-_putchar(text[i]);
-i++;
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 
 return (0);
