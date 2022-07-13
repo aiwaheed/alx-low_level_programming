c525c61a70a163d32e8ad48c74a03c779eeee28d
waheedibrahim <a.waheedibrahim@gmail.com>
2022-07-13 06:57:00 -0700

    comiit project 02

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
-* _strlen -> function to get the lenght of a string
-* @s: string pointer
-* Return: returns length of the string
+* _strlen - returns the length of a string
+* @s: string to find length of
+*
+* Return: length of s
 */
-
 int _strlen(char *s)
 {
-int i = 0;
+	int i = 0;
 
-while (*(s + 1))
-
-i++;
-
-return (i);
+	while (*(s + i))
+		i++;
+	return (i);
 }
