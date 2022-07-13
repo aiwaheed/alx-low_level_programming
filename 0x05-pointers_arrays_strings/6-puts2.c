4683373e8b411c520f856c75d6312c4a4ff79268
waheedibrahim <a.waheedibrahim@gmail.com>
2022-07-13 07:13:41 -0700

    commit of strings and array tasks

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,12 @@
 */
 void puts2(char *str)
 {
-	int i = 0;
+int i = 0;
 
-	while (*(str + i))
-	{
-		_putchar(*(str + i));
-		i = i + 2;
-	}
-	_putchar('\n');
+while (*(str + i))
+{
+_putchar(*(str + i));
+i = i + 2;
+}
+_putchar('\n');
 }
