7acd5a1b84a277c0240fdb8c2b45450354ec0f4f
waheedibrahim <a.waheedibrahim@gmail.com>
2022-07-13 06:45:30 -0700

    com tast 03

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,19 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-* _puts -> this is a function that shows puts
-* @str: a parameter to _puts function
+* _puts - prints a string in stdout
+* @str: string to print
+*
+* Return: void
 */
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
+	int i = 0;
+
+	while (*(str + i))
 	{
-		_putchar(*str);
+		_putchar(*(str + i));
+		i++;
 	}
-	_putchar('\0');
+	_putchar('\n');
 }
