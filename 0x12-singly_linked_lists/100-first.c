357cc33b66f4b14bb7591d25ae229cce4eaa7ee7
waheedibrahim <a.waheedibrahim@gmail.com>
2022-08-12 17:20:22 -0700

    commitinglinl_list files

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+void _constructor(void) __attribute__ ((constructor));
+
+/**
+ * _constructor - executes before main()
+ *
+ * Return: void
+ */
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
+Footer
+© 2022 Git
