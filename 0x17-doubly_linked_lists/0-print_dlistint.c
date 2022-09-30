9c0ff37eefafde147b75bc0744db22ac0b5ba2b5
aiwaheed <a.waheedibrahim@gmail.com>
2022-09-30 08:32:05 +0100

    commitsd

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,7 +3,8 @@
  *  * print_dlistint - Print all the values of each node in a `dlistint_t` list
  *   * @h: head pointer in doubly linked list
  *    * Return: Number of nodes in LL
- *     */
+ *     
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 		const dlistint_t *temp;
